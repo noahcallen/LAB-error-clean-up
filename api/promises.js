f8b25befaa59df30569b91d957b0bebdd89153f3
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2023-12-02 14:57:46 -0500

    almost working

---
+++ api/promises.js
@@ -0,0 +1,4 @@
+const jokeEndpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
+const postEndpoint = 'http://in-class-promise-e-default-rtdb.firebaseio.com';
+
+const getRequest = () => new Promise((resolve, ))
